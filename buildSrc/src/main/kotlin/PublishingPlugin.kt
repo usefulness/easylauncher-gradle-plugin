f03e5483d40c530b644aaac0e6c91bcb45b40cba
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-08-05 21:12:24 +0200

    Cleanup Github Actions

--- buildSrc/src/main/kotlin/PublishingPlugin.kt
+++ buildSrc/src/main/kotlin/PublishingPlugin.kt
@@ -19,7 +19,7 @@ class PublishingPlugin : Plugin<Project> {
             with(repositories) {
                 maven { maven ->
                     maven.name = "github"
-                    maven.setUrl("https://maven.pkg.github.com/mateuszkwiecinski/easylauncher-gradle-plugin")
+                    maven.setUrl("https://maven.pkg.github.com/usefulness/easylauncher-gradle-plugin")
                     with(maven.credentials) {
                         username = "mateuszkwiecinski"
                         password = findConfig("GITHUB_TOKEN")
@@ -40,7 +40,7 @@ class PublishingPlugin : Plugin<Project> {
                 repo = "maven"
                 name = "com.project.starter"
                 setLicenses("MIT")
-                vcsUrl = "https://github.com/mateuszkwiecinski/easylauncher-gradle-plugin.git"
+                vcsUrl = "https://github.com/usefulness/easylauncher-gradle-plugin.git"
                 with(version) {
                     name = project.version.toString()
                 }
@@ -55,5 +55,5 @@ class PublishingPlugin : Plugin<Project> {
 }
 
 private fun Project.findConfig(key: String): String {
-    return findProperty(key)?.toString() ?: System.getenv()[key] ?: ""
+    return findProperty(key)?.toString() ?: System.getenv(key) ?: ""
 }
