4485d54940a86dc773896907b0c5155e63a8743b
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-08-08 20:49:18 +0200

    Update screenshots

--- sample/record_all.sh
+++ sample/record_all.sh
@@ -1,14 +1,15 @@
 # utility script
-# I can't make the library work with multiple flavors :/ And as that temporairly solves my problem I'm going to leave it as it is
+# I can't make the library work with multiple flavors :/ And as that temporarily solves my problem I'm going to leave it as it is
 
 set -e
 function addToIndex {
   git status | grep "modified:" | awk '{print $2}' | xargs git add
+  git status | grep "screenshots" | grep -v "new file:"  | grep -v "deleted:" | awk '{print $1}' | xargs git add
 }
 
-echo "Record Start"
-./../gradlew :example-simple:recordDebugAndroidTestScreenshotTest
-addToIndex
+#echo "Record Start"
+#./../gradlew :example-simple:recordDebugAndroidTestScreenshotTest
+#addToIndex
 echo "Simple completed"
 ./../gradlew :example-custom:recordLocalAaaDebugAndroidTestScreenshotTest
 addToIndex
