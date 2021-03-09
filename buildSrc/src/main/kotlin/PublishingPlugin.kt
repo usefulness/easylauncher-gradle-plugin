9132ed488caf2a72002f8b340a3f92e90900f89a
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-03-09 18:30:00 +0100

    Disable Bintray publishing

--- buildSrc/src/main/kotlin/PublishingPlugin.kt
+++ buildSrc/src/main/kotlin/PublishingPlugin.kt
@@ -1,4 +1,3 @@
-import com.jfrog.bintray.gradle.BintrayExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
@@ -9,7 +8,6 @@ import org.gradle.api.publish.maven.MavenPublication
 class PublishingPlugin : Plugin<Project> {
 
     override fun apply(target: Project) = with(target) {
-        pluginManager.apply("com.jfrog.bintray")
         pluginManager.apply("maven-publish")
 
         extensions.configure<JavaPluginExtension> {
@@ -31,21 +29,6 @@ class PublishingPlugin : Plugin<Project> {
                     it.from(components.getByName("java"))
                 }
             }
-        }
-
-        extensions.configure<BintrayExtension> {
-            user = findConfig("BINTRAY_USER")
-            key = findConfig("BINTRAY_KEY")
-            with(pkg) {
-                repo = "maven"
-                name = "com.project.starter"
-                setLicenses("MIT")
-                vcsUrl = "https://github.com/usefulness/easylauncher-gradle-plugin.git"
-                with(version) {
-                    name = project.version.toString()
-                }
-            }
-            setPublications("mavenJava")
         }
     }
 
