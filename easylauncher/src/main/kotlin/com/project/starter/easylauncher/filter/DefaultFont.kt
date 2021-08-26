9a0e68aa9b5c2132800513dc1ea27bf8467b93c9
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-08-26 22:02:51 +0300

    wip

--- easylauncher/src/main/kotlin/com/project/starter/easylauncher/filter/DefaultFont.kt
+++ easylauncher/src/main/kotlin/com/project/starter/easylauncher/filter/DefaultFont.kt
@@ -4,9 +4,10 @@ import java.awt.Font
 
 internal val DEFAULT_EASYLAUNCHER_FONT: Font by lazy {
     EasyLauncherFilter::class.java.classLoader
-        .getResourceAsStream("Roboto-Regular.ttf")!!
-        .buffered()
+        .getResourceAsStream("Roboto-Regular.ttf")
         .use {
-            Font.createFont(Font.TRUETYPE_FONT, it)
+            runCatching { Font.createFont(Font.TRUETYPE_FONT, it) }
+                .getOrNull()
+                ?: error("Couldn't load bundled font. Make sure you have font support installed.\nhttps://github.com/usefulness/easylauncher-gradle-plugin/issues/201")
         }
 }
