2382e00be7072fda3af26f26de3d16d6bd6906d3
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2022-08-08 00:56:02 +0100

    Install fixed Pillow version

--- sample/record_all.sh
+++ sample/record_all.sh
@@ -9,73 +9,72 @@ function addToIndex {
 }
 
 echo "Mutli Activity Alias start"
-./../gradlew :example-activity-alias:recordDebugAndroidTestScreenshotTest
+./../gradlew :example-activity-alias:recordDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-activity-alias:recordUnspecifiedAndroidTestScreenshotTest \-PtestBuildType=unspecified
+./../gradlew :example-activity-alias:recordUnspecifiedAndroidTestScreenshotTest \-PtestBuildType=unspecified --no-configuration-cache
 addToIndex
-./../gradlew :example-activity-alias:recordReleaseAndroidTestScreenshotTest \-PtestBuildType=release
+./../gradlew :example-activity-alias:recordReleaseAndroidTestScreenshotTest \-PtestBuildType=release --no-configuration-cache
 addToIndex
 
 echo "Custom Start"
-./../gradlew :example-custom:recordLocalAaaDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordLocalAaaDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-custom:recordProductionAaaDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordProductionAaaDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-custom:recordQaAaaDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordQaAaaDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-custom:recordStagingAaaDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordStagingAaaDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-custom:recordLocalBbbDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordLocalBbbDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-custom:recordProductionBbbDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordProductionBbbDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-custom:recordQaBbbDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordQaBbbDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-custom:recordStagingBbbDebugAndroidTestScreenshotTest
+./../gradlew :example-custom:recordStagingBbbDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
 
 echo "Icon round Start"
-./../gradlew :example-icon-round:recordKeepDebugAndroidTestScreenshotTest
+./../gradlew :example-icon-round:recordKeepDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-icon-round:recordIgnoreDebugAndroidTestScreenshotTest
+./../gradlew :example-icon-round:recordIgnoreDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
 
 echo "Library Start"
-./../gradlew :example-library:recordDebugAndroidTestScreenshotTest
+./../gradlew :example-library:recordDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
 
 echo "Manifest Placeholder Start"
-./../gradlew :example-manifest-placeholder:recordDebugAndroidTestScreenshotTest
+./../gradlew :example-manifest-placeholder:recordDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
 
 echo "Scripted Start"
-./../gradlew :example-scripted:recordDebugAndroidTestScreenshotTest
+./../gradlew :example-scripted:recordDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
 
 echo "Simple Start"
-./../gradlew :example-simple:recordDebugAndroidTestScreenshotTest
+./../gradlew :example-simple:recordDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
 
 echo "Vector start"
-./../gradlew :example-vector:recordTwoVectorsDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordTwoVectorsDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordWrongLabelPositionDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordWrongLabelPositionDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordChromeLikeDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordChromeLikeDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordChromeLikeCustomizedDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordChromeLikeCustomizedDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordChromeLikeTopDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordChromeLikeTopDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordAlphaDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordAlphaDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordCustomFontDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordCustomFontDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordCustomFontByFileDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordCustomFontByFileDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordCustomFontByOtfDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordCustomFontByOtfDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
-./../gradlew :example-vector:recordUsingAndroidVersionDebugAndroidTestScreenshotTest
+./../gradlew :example-vector:recordUsingAndroidVersionDebugAndroidTestScreenshotTest --no-configuration-cache
 addToIndex
 echo "Completed"
-
