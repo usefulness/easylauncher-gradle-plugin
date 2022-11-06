5f94bd4a7c0fe00b2ed7ceb1d59e150cad75e1c2
Kristoffer Kleine <kristoffer.kleine@gmail.com>
2022-11-06 12:29:34 +0100

    Port plugin to AndroidComponentsExtension
    This fixes #349

--- easylauncher/src/main/kotlin/com/project/starter/easylauncher/plugin/EasyLauncherTask.kt
+++ easylauncher/src/main/kotlin/com/project/starter/easylauncher/plugin/EasyLauncherTask.kt
@@ -3,51 +3,52 @@ package com.project.starter.easylauncher.plugin
 import com.project.starter.easylauncher.filter.EasyLauncherFilter
 import com.project.starter.easylauncher.plugin.models.AdaptiveIcon
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.file.*
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.SetProperty
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.PathSensitive
-import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 import javax.inject.Inject
 import kotlin.system.measureTimeMillis
 
 @CacheableTask
-open class EasyLauncherTask @Inject constructor(
-    private val objectFactory: ObjectFactory,
+abstract class EasyLauncherTask @Inject constructor(
+    private val objects: ObjectFactory
 ) : DefaultTask() {
 
-    @OutputDirectory
-    val outputDir: RegularFileProperty = objectFactory.fileProperty()
+    @get:Input
+    abstract val manifestFiles: ListProperty<File>
 
-    @Input
-    val filters: ListProperty<EasyLauncherFilter> = listProperty<EasyLauncherFilter>()
+    @get:Input
+    abstract val manifestPlaceholders: MapProperty<String, String>
 
-    @Input
-    val minSdkVersion: Property<Int> = property(default = null)
+    @get:Input
+    abstract val resourceDirectories: SetProperty<File>
 
-    @InputFiles
-    @PathSensitive(PathSensitivity.RELATIVE)
-    val resourceDirectories: ConfigurableFileCollection = objectFactory.fileCollection()
+    @get:Input
+    abstract val filters: ListProperty<EasyLauncherFilter>
 
-    @InputFiles
-    @PathSensitive(PathSensitivity.RELATIVE)
-    val icons: ConfigurableFileCollection = objectFactory.fileCollection()
+    @get:Input
+    abstract val customIconNames: SetProperty<String>
+
+    @get:Input
+    abstract val minSdkVersion: Property<Int>
+
+    @get:OutputDirectory
+    abstract val outputDir: DirectoryProperty
 
     @TaskAction
     fun run() {
-        if (filters.get().isEmpty()) {
-            return
-        }
-
         val taskExecutionTime = measureTimeMillis {
+            val iconNames = customIconNames.get().takeIf { it.isNotEmpty() } ?: getLauncherIconNames()
+            val icons = getIcons(iconNames)
+
             icons.forEach { iconFile ->
                 val adaptiveIcon = iconFile.asAdaptiveIcon()
                 if (adaptiveIcon == null) {
@@ -62,34 +63,48 @@ open class EasyLauncherTask @Inject constructor(
         logger.info("task finished in $taskExecutionTime ms")
     }
 
-    private fun processIcon(adaptiveIcon: AdaptiveIcon) {
-        resourceDirectories.forEach { resDir ->
-            val icons = objectFactory.getIconFiles(parent = resDir, iconName = adaptiveIcon.foreground)
-            icons.forEach { iconFile ->
-                val outputFile = iconFile.getOutputFile()
-                if (iconFile.extension == "xml") {
-                    iconFile.transformXml(outputFile, minSdkVersion.get(), filters.get())
-                } else {
-                    iconFile.transformImage(outputFile, filters.get(), adaptive = true)
-                }
+    private fun getLauncherIconNames(): Set<String> {
+        return manifestFiles
+            .get()
+            .filter { it.exists() }
+            .map {
+                it.getLauncherIcons(manifestPlaceholders.get())
             }
-        }
+            .flatten()
+            .toSet()
     }
 
-    private inline fun <reified T> listProperty(default: Iterable<T> = emptyList()) =
-        objectFactory.listProperty(T::class.java).apply {
-            set(default)
-        }
+    private fun getIcons(iconNames: Set<String>): List<File> {
+        logger.info("will process icons: ${iconNames.joinToString()}")
 
-    private inline fun <reified T> property(default: T? = null) =
-        objectFactory.property(T::class.java).apply {
-            set(default)
-        }
+        return resourceDirectories
+            .get()
+            .filter {
+                it.exists()
+            }
+            .flatMap { resDir ->
+                iconNames.flatMap {
+                    objects.getIconFiles(parent = resDir, iconName = it)
+                }
+            }
+    }
+
+    private fun processIcon(adaptiveIcon: AdaptiveIcon) {
+        resourceDirectories
+            .get()
+            .forEach { resDir ->
+                val icons = objects.getIconFiles(parent = resDir, iconName = adaptiveIcon.foreground)
+                icons.forEach { iconFile ->
+                    val outputFile = iconFile.getOutputFile()
+                    if (iconFile.extension == "xml") {
+                        iconFile.transformXml(outputFile, minSdkVersion.get(), filters.get())
+                    } else {
+                        iconFile.transformImage(outputFile, filters.get(), adaptive = true)
+                    }
+                }
+            }
+    }
 
     private fun File.getOutputFile(): File =
         File(outputDir.asFile.get(), "${parentFile.name}/$name")
-
-    companion object {
-        const val NAME = "easylauncher"
-    }
 }
