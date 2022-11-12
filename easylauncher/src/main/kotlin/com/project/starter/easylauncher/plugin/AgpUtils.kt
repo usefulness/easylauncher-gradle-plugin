93c15e201059c64bcbc1c6cc590c6585b6c1c446
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2022-11-12 07:56:42 +0100

    Remove unused utilities

--- easylauncher/src/main/kotlin/com/project/starter/easylauncher/plugin/AgpUtils.kt
+++ easylauncher/src/main/kotlin/com/project/starter/easylauncher/plugin/AgpUtils.kt
@@ -1,28 +1,7 @@
 package com.project.starter.easylauncher.plugin
 
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.api.BaseVariant
-import org.gradle.api.DomainObjectSet
-import org.gradle.api.Project
 import org.gradle.api.model.ObjectFactory
 import java.io.File
-
-private val supportedPlugins = listOf(
-    "com.android.application",
-    "com.android.library",
-)
-
-internal fun Project.configureSupportedPlugins(block: (DomainObjectSet<out BaseVariant>) -> Unit) {
-    supportedPlugins.forEach { pluginId ->
-        pluginManager.withPlugin(pluginId) { block(findVariants()) }
-    }
-}
-
-internal fun Project.findVariants(): DomainObjectSet<out BaseVariant> =
-    extensions.findByType(AppExtension::class.java)?.applicationVariants
-        ?: extensions.findByType(LibraryExtension::class.java)?.libraryVariants
-        ?: this.objects.domainObjectSet(BaseVariant::class.java)
 
 internal fun ObjectFactory.getIconFiles(parent: File, iconName: String): Iterable<File> =
     fileTree().from(parent).apply {
